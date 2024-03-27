07bae3853014020b41e523cea1c1241c8be829a8
Bogdan <bsshul@gmail.com>
2024-03-27 20:51:53 +0100

    Fix carousel

--- components/promo.tsx
+++ components/promo.tsx
@@ -12,17 +12,17 @@ export default function Promo({ }: Props) {
   <div className='flex flex-col md:flex-row gap-3'>
    <Carousel options={OPTIONS} />
 
-   {/* <div className='md:w-1/2 rounded-xl'>
+   <div className='md:w-1/2 rounded-xl'>
     <Image
      src={"/2.png"}
      alt={"Img"}
      width={408}
      height={300}
-     className="w-full object-cover rounded-lg"
+     className="w-full rounded-lg"
      priority
      quality={100}
     />
-   </div> */}
+   </div>
   </div>
  )
 }
