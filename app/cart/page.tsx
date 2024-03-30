b1909ffd34d38bf15780f348bafa5706e607a7ab
Bogdan <bsshul@gmail.com>
2024-03-30 10:18:21 +0100

    Fix

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -7,114 +7,124 @@ import { PRODUCTS } from '@/data';
 type Props = {}
 
 const order = {
- id: 1,
- customer: {
   id: 1,
-  name: "Bogdan",
-  adress: "Poznanska 14a"
- },
- products: [
-  { id: 1, productId: 1, quantity: 1 },
-  { id: 2, productId: 3, quantity: 3 },
- ],
- total: 150
+  customer: {
+    id: 1,
+    name: "Bogdan",
+    adress: "Poznanska 14a"
+  },
+  products: [
+    { id: 1, productId: 1, quantity: 1 },
+    { id: 2, productId: 3, quantity: 3 },
+    { id: 3, productId: 2, quantity: 2 },
+  ],
+  total: 150
 }
 
 export default function Cart({ }: Props) {
 
- const currentProduct = (id: number) => {
-  return PRODUCTS.filter(product => product.id === id)[0]
- }
+  const currentProduct = (id: number) => {
+    return PRODUCTS.filter(product => product.id === id)[0]
+  }
 
- return (
-  <section>
-   <div className="wrapper pb-5">
-    <h2>Cart</h2>
+  return (
+    <section>
+      <div className="wrapper pb-5">
+        <h2>Cart</h2>
 
-    <section className='mt-4 flex flex-col lg:flex-row'>
-     <div className='p-4 w-full lg:w-2/3 bg-white rounded-t-lg lg:rounded-r-none lg:rounded-l-xl flex flex-col items-start md:flex-row gap-8'>
-      <div className='w-full'>
-       <b>Products</b>
+        <section className='mt-4 flex flex-col lg:flex-row'>
+          <div className='p-4 w-full lg:w-2/3 bg-white rounded-t-lg lg:rounded-r-none lg:rounded-l-xl flex flex-col items-start md:flex-row gap-8'>
+            <div className='w-full'>
+              <b>Products</b>
 
-       <div className='mt-4 flex flex-col gap-3'>
-        {order.products.map((product, i) => (
-         <div key={product.id} className='flex flex-col md:flex-row gap-4 items-center border-b pb-3 last:border-none'>
-          <div className='w-full flex gap-4 items-center'>
-           <Image
-            src={currentProduct(product.id).images[0]}
-            alt={"Img"}
-            width={408}
-            height={100}
-            className="w-32 h-32 object-contain bg-green-500 rounded-lg"
-            priority
-            quality={100}
-           />
+              <div className='mt-4 flex flex-col gap-3'>
+                {order.products.map((product, i) => (
+                  <div key={product.id} className='flex flex-col md:flex-row gap-4 items-center border-b pb-3 last:border-none'>
+                    <div className='w-full flex gap-4 md:items-center'>
+                      <Image
+                        src={currentProduct(product.id).images[0]}
+                        alt={"Img"}
+                        width={408}
+                        height={100}
+                        className="w-32 h-32 object-contain bg-green-500 rounded-lg"
+                        priority
+                        quality={100}
+                      />
 
-           <div className='w-full'>
-            <b>{currentProduct(product.id).title}</b>
-            <p>{currentProduct(product.id).price} zl</p>
-           </div>
+                      <div className='w-full'>
+                        <b>{currentProduct(product.id).title}</b>
+                        <p>{currentProduct(product.id).price} zl</p>
+                      </div>
+                    </div>
+
+                    <div className='w-full flex gap-4 items-center justify-between'>
+
+                      <div className='max-w-52 w-full flex justify-between border-2 rounded-xl'>
+                        <button
+                          // onClick={() => setCount(count - 1)}
+                          className='h-12 w-12 aspect-square font-bold'>
+                          -
+                        </button>
+
+                        <input
+                          type='number'
+                          value={order.products.filter(el => el.id === product.id)[0].quantity}
+                          min={1}
+                          step={1}
+                          // value={count}
+                          // onChange={(e: any) => setCount(e.target.value)}
+                          className='w-full text-center font-bold border-x-2 flex items-center justify-center' />
+
+                        <button
+                          // onClick={() => setCount(count + 1)}
+                          className='h-12 w-12 aspect-square font-bold'>
+                          +
+                        </button>
+                      </div>
+
+                      <b className='px-4 whitespace-nowrap'>
+                        {order.products.filter(el => el.id === product.id)[0].quantity * currentProduct(product.id).price}
+                      </b>
+
+                      <button
+                        className='w-9 h-9 bg-red-50 flex items-center justify-center rounded-lg active:scale-95'>
+                        <X />
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+            </div>
+
           </div>
 
-          <div className='w-full flex gap-4 items-center justify-between'>
+          <div className='w-full lg:w-1/3 min-w-[300px] bg-gray-100 p-4 rounded-b-lg lg:rounded-r-xl lg:rounded-l-none'>
+            <div className='w-full flex items-center justify-between'>
+              <b>Summary</b>
+              <span>{order.products.length} items</span>
+            </div>
 
-           <div className='mt-4 w-56 flex justify-between border-2 rounded-xl'>
+            <p className='font-bold mt-4 text-xs'>Customer</p>
+            <p>{order.customer.name}</p>
+
+            <p className='font-bold mt-4 text-xs'>Adres</p>
+            <p>{order.customer.adress}</p>
+
+            <div className='flex justify-between items-center'>
+              <h2>Total:</h2>
+              <h2>{order.total} zl</h2>
+            </div>
+
             <button
-             // onClick={() => setCount(count - 1)}
-             className='py-4 px-6 font-bold'>
-             -
+              className={cn("w-full bg-black disabled:bg-gray-400 text-white font-bold text-lg flex items-center justify-center px-2 py-4 rounded-lg",
+                true && "mt-4")}
+            >
+              Buy
             </button>
-
-            <input
-             type='number'
-             min={1}
-             step={1}
-             // value={count}
-             // onChange={(e: any) => setCount(e.target.value)}
-             className='w-full text-center font-bold px-10 border-x-2 flex items-center justify-center' />
-
-            <button
-             // onClick={() => setCount(count + 1)}
-             className='py-4 px-6 font-bold'>
-             +
-            </button>
-           </div>
-
-           <b className='px-4 whitespace-nowrap'>145 zl</b>
-
-           <button><X /></button>
           </div>
-         </div>
-        ))}
-       </div>
-
+        </section>
       </div>
-
-     </div>
-
-     <div className='w-full lg:w-1/3 min-w-[300px] bg-gray-100 p-4 rounded-b-lg lg:rounded-r-xl lg:rounded-l-none'>
-      <div className='w-full flex items-center justify-between'>
-       <b>Summary</b>
-       <span>{order.products.length} items</span>
-      </div>
-
-      <p className='font-bold mt-4 text-xs'>Customer</p>
-      <p>{order.customer.name}</p>
-
-      <p className='font-bold mt-4 text-xs'>Adres</p>
-      <p>{order.customer.adress}</p>
-
-      <h2>{order.total} zl</h2>
-
-      <button
-       className={cn("w-full bg-black disabled:bg-gray-400 text-white font-bold text-lg flex items-center justify-center px-2 py-4 rounded-lg",
-        true && "mt-4")}
-      >
-       Buy
-      </button>
-     </div>
     </section>
-   </div>
-  </section>
- )
+  )
 }
