24076415a461bb548704149f6abad2c79df03d52
Bogdan <bsshul@gmail.com>
2024-04-01 12:28:50 +0200

    Fix

--- components/categories.tsx
+++ components/categories.tsx
@@ -36,20 +36,20 @@ export default function Categories() {
    </button>
 
    <Transition appear show={isOpen} as={Fragment}>
-    <Dialog as="div" className="relative z-10" onClose={closeModal}>
-     <Transition.Child
-      as={Fragment}
-      enter="ease-out duration-300"
-      enterFrom="opacity-0"
-      enterTo="opacity-100"
-      leave="ease-in duration-200"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
-     >
-      <div className="fixed inset-0 bg-black/50" />
-     </Transition.Child>
+    <Dialog as="div" className="fixed inset-0 wrapper" onClose={closeModal}>
+     <div className="relative max-w-xs mr-auto">
+      <Transition.Child
+       as={Fragment}
+       enter="ease-out duration-300"
+       enterFrom="opacity-0"
+       enterTo="opacity-100"
+       leave="ease-in duration-200"
+       leaveFrom="opacity-100"
+       leaveTo="opacity-0"
+      >
+       <div className="fixed inset-0 bg-black/50" />
+      </Transition.Child>
 
-     <div className="fixed top-28 left-0 max-w-sm w-full">
       <div className="flex min-h-full items-center justify-center p-4 text-center">
        <Transition.Child
         as={Fragment}
@@ -60,7 +60,7 @@ export default function Categories() {
         leaveFrom="opacity-100 scale-100 translate-y-0"
         leaveTo="opacity-0 scale-95 -translate-y-2"
        >
-        <Dialog.Panel className="w-full transform overflow-hidden rounded-lg bg-white text-left align-middle shadow-xl transition-all">
+        <Dialog.Panel className="absolute top-32 left-0 w-full transform overflow-hidden rounded-lg bg-white text-left align-middle shadow-xl transition-all">
 
          <Link
           href={{
